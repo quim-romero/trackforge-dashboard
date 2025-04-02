918f5c97e3df537cc68ab31ae7317f5875e19596
quim-romero <quimromero22@gmail.com>
2025-04-02 13:25:31 +0200

    Dashboard view & base HTML structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<div>Dashboard</div>} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/tasks" element={<div>Tasks</div>} />
           <Route path="/stats" element={<div>Stats</div>} />
           <Route path="/profile" element={<div>Profile</div>} />
