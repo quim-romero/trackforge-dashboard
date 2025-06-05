99cee8df5a3f723d092212cd2acec69dea20d1ff
quim-romero <quimromero22@gmail.com>
2025-06-05 15:18:56 +0200

    Fix bug

--- src/store/useTaskStore.ts
+++ src/store/useTaskStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { nanoid } from "nanoid";
-import type { Task, Priority } from "../types";
+import type { Task } from "../types";
 
 interface TaskState {
   tasks: Task[];
